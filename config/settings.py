03989eed9c51bcb6cc1bd04936d8375833f8473a
jun <allensiri@naver.com>
2021-02-08 17:52:49 +0900

    add site id

--- config/settings.py
+++ config/settings.py
@@ -141,3 +141,6 @@ DATABASES['default'].update(db_from_env)
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+SITE_ID=1
+
